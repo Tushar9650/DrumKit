b81f88c2e4483961667918dd66259893f48f41ba
TUSHAR SINGHOTA <78820730+Tushar9650@users.noreply.github.com>
2022-09-21 19:47:57 +0530

    Update index.js

--- index.js
+++ index.js
@@ -17,37 +17,37 @@ for(var i=0;i<document.querySelectorAll("button").length;i++){
             case "w":
                 var audio = new Audio("sounds/tom-1.mp3");
                 audio.play();
-                document.querySelector("#title").innerHTML="😊";
+                document.querySelector("#title").innerHTML="Tuu!!";
                 break;
             case "a":
                 var audio = new Audio("sounds/tom-2.mp3");
                 audio.play();
-                document.querySelector("#title").innerHTML="😊";
+                document.querySelector("#title").innerHTML="Toh!!";
                 break;
             case "s":
                 var audio = new Audio("sounds/tom-3.mp3");
                 audio.play();
-                document.querySelector("#title").innerHTML="😊";
+                document.querySelector("#title").innerHTML="Doh!!";
                 break;
             case "d":
                 var audio = new Audio("sounds/tom-4.mp3");
                 audio.play();
-                document.querySelector("#title").innerHTML="😊";
+                document.querySelector("#title").innerHTML="Dooooohhh😁";
                 break;
             case "j":
                 var audio = new Audio("sounds/snare.mp3");
                 audio.play();
-                document.querySelector("#title").innerHTML="😊";
+                document.querySelector("#title").innerHTML="TTTTT";
                 break;
             case "k":
                 var audio = new Audio("sounds/crash.mp3");
                 audio.play();
-                document.querySelector("#title").innerHTML="😊";
+                document.querySelector("#title").innerHTML="😂cchhhhhhh";
                 break;
             case "l":
                 var audio = new Audio("sounds/kick-bass.mp3");
                 audio.play();
-                document.querySelector("#title").innerHTML="😊";
+                document.querySelector("#title").innerHTML="Dmmmmm🤷‍♀️";
                 break;
                             
 
